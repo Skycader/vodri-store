d7a6f91361588546f89dbe167993875fd12b72df
d.ustimenko <daniil.ustimencko@gmail.com>
2023-08-23 12:45:32 +0300

    added sidenav

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { WelcomeBadgeComponent } from './components/welcome-badge/welcome-badge.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 @NgModule({
-  declarations: [WelcomeBadgeComponent, MainLayoutComponent],
+  declarations: [WelcomeBadgeComponent, MainLayoutComponent, SideNavComponent],
   imports: [CommonModule, SharedModule],
-  exports: [WelcomeBadgeComponent, MainLayoutComponent],
+  exports: [WelcomeBadgeComponent, MainLayoutComponent, SideNavComponent],
 })
 export class MainModule {}
